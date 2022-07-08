b59c03c2f0ab20bae6c0527c572230a86a732529
luisboto <43352462+LuisBoto@users.noreply.github.com>
2022-07-08 21:24:57 +0200

    Payments now correctly shown on frontend

--- frontend/sharedPaymentsWeb/src/app/app.component.ts
+++ frontend/sharedPaymentsWeb/src/app/app.component.ts
@@ -18,10 +18,12 @@ export class AppComponent {
     })
   };
   users: User[] = [];
+  payments: Payment[] = [];
 
   constructor(private http: HttpClient) {
     this.http = http;
     this.readUserList();
+    this.readPaymentList();
   }
 
   readUserList() {
@@ -33,6 +35,21 @@ export class AppComponent {
     .subscribe({
       next: (response) => {
         this.users = (response as []);
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+  readPaymentList() {
+    this.http
+    .get(
+      this.apiUrl+'/payments', 
+      this.httpOptions
+    )
+    .subscribe({
+      next: (response) => {
+        this.payments = (response as []);
+        this.formatPayments();
       },
       error: (error) => console.log(error),
     });
@@ -60,15 +77,33 @@ export class AppComponent {
         this.httpOptions
       )
       .subscribe({
-        next: (response) => this.readUserList(),
+        next: (response) => {
+          this.readUserList();
+          this.readPaymentList();
+        },
         error: (error) => console.log(error),
       });
   }
 
+  formatPayments() {
+    for (let i = 0; i < this.payments.length; i++) {
+      this.payments[i].paymentDate = new Date(this.payments[i].paymentDate).toLocaleString();
+      this.payments[i].payerName = this.users.find(user => user.id == this.payments[i].payerId)?.name;
+    }
+  }
 }
 
 export class User {
   debt!: number;
   name!: string;
   id!: string;
+}
+
+export class Payment {
+  price!: number;
+  paymentDate!: string;
+  payerId!: string;
+  description!: string;
+  id!: string;
+  payerName!: string | undefined;
 }
