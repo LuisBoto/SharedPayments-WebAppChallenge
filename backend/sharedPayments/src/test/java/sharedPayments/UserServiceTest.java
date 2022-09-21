b829a57efba4e53abdc3dfe7a445481e8b01d64a
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2022-09-21 09:33:29 +0200

    tests: added getDebt method on userService tests

--- backend/sharedPayments/src/test/java/sharedPayments/UserServiceTest.java
+++ backend/sharedPayments/src/test/java/sharedPayments/UserServiceTest.java
@@ -53,6 +53,10 @@ public class UserServiceTest {
 				});
 	}
 	
+	private String getDebtForMockUser(Long Id) {
+		return this.userRepository.findById(Id).get().getBDDebt().toString();
+	}
+	
 	@Test
 	void givenTwoUsers_WhenGetAllUsers_ThenSizeIsTwo() {
 		when(this.userRepository.findAll()).thenReturn( Arrays.asList(
@@ -91,43 +95,43 @@ public class UserServiceTest {
 	void givenOneUser_WhenUpdateUserDebts_ThenDebtIsUnchanged() {
 		this.setUpMockRepositoryUsersWithDebts(100D);
 		this.userService.updateUserDebts(1L, 300D);
-		assertEquals("100.00", this.userRepository.findById(1L).get().getBDDebt().toString());
+		assertEquals("100.00", this.getDebtForMockUser(1L));
 	}
 	
 	@Test
 	void givenTwoUsers_WhenUpdateUserDebts_ThenDebtIsDistributed() {
 		this.setUpMockRepositoryUsersWithDebts(-100D, 200D);
 		this.userService.updateUserDebts(2L, 80D);
-		assertEquals("-60.00", this.userRepository.findById(1L).get().getBDDebt().toString());
-		assertEquals("160.00", this.userRepository.findById(2L).get().getBDDebt().toString());
+		assertEquals("-60.00", this.getDebtForMockUser(1L));
+		assertEquals("160.00", this.getDebtForMockUser(2L));
 	}
 	
 	@Test
 	void givenTwoUsers_WhenUpdateUserDebtsWithBigPrice_ThenDebtsProportionallyReverse() {
 		this.setUpMockRepositoryUsersWithDebts(-45.60, 51.99);
 		this.userService.updateUserDebts(2L, 149.77);
-		assertEquals("29.29", this.userRepository.findById(1L).get().getBDDebt().toString());
-		assertEquals("-22.90", this.userRepository.findById(2L).get().getBDDebt().toString());
+		assertEquals("29.29", this.getDebtForMockUser(1L));
+		assertEquals("-22.90", this.getDebtForMockUser(2L));
 	}
 	
 	@Test
 	void givenThreeUsers_WhenUpdateUserDebtsWithIndivisiblePrice_ThenRemainderIsAssigned() {
 		this.setUpMockRepositoryUsersWithDebts(0D, 0D, 0D);
 		this.userService.updateUserDebts(2L, 100D);
-		assertEquals("33.34", this.userRepository.findById(1L).get().getBDDebt().toString());
-		assertEquals("-66.67", this.userRepository.findById(2L).get().getBDDebt().toString());
-		assertEquals("33.33", this.userRepository.findById(3L).get().getBDDebt().toString());
+		assertEquals("33.34", this.getDebtForMockUser(1L));
+		assertEquals("-66.67", this.getDebtForMockUser(2L));
+		assertEquals("33.33", this.getDebtForMockUser(3L));
 	}
 	
 	@Test
 	void givenFiveUsers_WhenUpdateUserDebtsWithIndivisibleDecimalPrice_ThenRemainderIsAssigned() {
 		this.setUpMockRepositoryUsersWithDebts(0D, 0D, 0D, 0D, 0D);
 		this.userService.updateUserDebts(5L, 100.03D);
-		assertEquals("20.01", this.userRepository.findById(1L).get().getBDDebt().toString());
-		assertEquals("20.01", this.userRepository.findById(2L).get().getBDDebt().toString());
-		assertEquals("20.01", this.userRepository.findById(3L).get().getBDDebt().toString());
-		assertEquals("20.00", this.userRepository.findById(4L).get().getBDDebt().toString());
-		assertEquals("-80.03", this.userRepository.findById(5L).get().getBDDebt().toString());
+		assertEquals("20.01", this.getDebtForMockUser(1L));
+		assertEquals("20.01", this.getDebtForMockUser(2L));
+		assertEquals("20.01", this.getDebtForMockUser(3L));
+		assertEquals("20.00", this.getDebtForMockUser(4L));
+		assertEquals("-80.03", this.getDebtForMockUser(5L));
 	}
 
 }
