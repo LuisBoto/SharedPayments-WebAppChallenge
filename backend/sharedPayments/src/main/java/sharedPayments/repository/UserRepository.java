689c21ee24054fb439d87c783eee2428b0363577
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2022-09-22 11:37:08 +0200

    tests: testResources not working

--- backend/sharedPayments/src/main/java/sharedPayments/repository/UserRepository.java
+++ backend/sharedPayments/src/main/java/sharedPayments/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package sharedPayments.repository;
 
+import io.micronaut.context.annotation.Requires;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.CrudRepository;
 import sharedPayments.model.User;
 
 @Repository
+@Requires(env = {"dev", "prod"})
 public interface UserRepository extends CrudRepository<User, Long> {
 
 }
--- backend/sharedPayments/src/test/java/sharedPayments/integration/RepositoryIT.java
+++ backend/sharedPayments/src/test/java/sharedPayments/integration/RepositoryIT.java
@@ -6,13 +6,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.naming.NamingException;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.annotation.Value;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
@@ -21,26 +19,26 @@ import sharedPayments.repository.PaymentRepository;
 import sharedPayments.repository.UserRepository;
 
 @MicronautTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class RepositoryIT {
-	
-	@Value("${datasources.default.username}")
+
+	@Value("${datasources.mysql.username}")
 	private static String username;
-	@Value("${datasources.default.password}")
+	@Value("${datasources.*.password}")
 	private static String password;
 	@Value("${datasources.default.url}")
 	private static String url;
 	private Map<String, String> dbConfig = new HashMap<String, String>();
+
 	
-	@Inject
 	private UserRepository userRepository;
-	@Inject
+	
 	private PaymentRepository paymentRepository;
-	
-	@BeforeAll
-	static void setUpDBContainer() throws NamingException {
-		//mySQL.start();
+
+	public RepositoryIT() {
+		ApplicationContext.run("test");
 	}
-	
+
 	@BeforeEach
 	void resetDB() {
 		this.dbConfig.put("url", url);
@@ -49,17 +47,12 @@ public class RepositoryIT {
 		QueryEnum.EMPTY_DATABASE.execute(dbConfig);
 		QueryEnum.CREATE_USER_TABLE.execute(dbConfig);
 		QueryEnum.CREATE_PAYMENT_TABLE.execute(dbConfig);
-	}
-	
-	@AfterAll
-	static void closeDBContainer() {
-		//mySQL.close();
-		//mySQL.stop();
+
 	}
 
 	@Test
 	void notImplemented() {
-		System.out.println(this.userRepository.save(new User("Fuencisla")));
+		//System.out.println(this.userRepository.save(new User("Fuencisla")));
 		assertThat(1, is(1));
 	}
 
