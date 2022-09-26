020a39c4f7819af6622bc013a54a0a7103dc376a
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2022-09-26 15:39:33 +0200

    tests: fixed test properties profile

--- backend/sharedPayments/src/main/java/sharedPayments/Application.java
+++ backend/sharedPayments/src/main/java/sharedPayments/Application.java
@@ -1,12 +1,9 @@
 package sharedPayments;
 
-import io.micronaut.context.ApplicationContext;
-import io.micronaut.context.env.Environment;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
 	public static void main(String[] args) {
-		ApplicationContext.run(Environment.DEVELOPMENT);
 		Micronaut.run(Application.class, args);
 	}
 }
