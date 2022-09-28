3a559ecbec34d52592543c005b1c249c7863e02f
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2022-09-28 13:26:32 +0200

    tests: api rest test for invalid user post

--- backend/sharedPayments/src/test/java/sharedPayments/integration/ControllerIT.java
+++ backend/sharedPayments/src/test/java/sharedPayments/integration/ControllerIT.java
@@ -15,8 +15,6 @@ import io.micronaut.http.HttpStatus;
 import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import io.restassured.specification.RequestSpecification;
-import jakarta.inject.Inject;
-import sharedPayments.controller.UserRestController;
 import sharedPayments.model.User;
 import sharedPayments.model.dto.UserDto;
 import sharedPayments.service.UserService;
@@ -24,16 +22,14 @@ import sharedPayments.service.UserService;
 @MicronautTest
 public class ControllerIT {
 	
-	@Inject
-	private UserRestController userController;
-	
 	@MockBean(UserService.class)
     public UserService dependency() {
 		var userService = mock(UserService.class);
-		when(userService.getUsers()).thenReturn(Arrays.asList(
+		var users = Arrays.asList(
 				new UserDto(1L, "Pepe", 18.0),
 				new UserDto(2L, "John", 53.99),
-				new UserDto(3L, "Maria", 14.56)));
+				new UserDto(3L, "Maria", 14.56));
+		when(userService.getUsers()).thenReturn(users);
 		when(userService.createUser(any(UserDto.class))).thenAnswer(call -> {
 				UserDto paramUser = call.getArgument(0);
 				User user = paramUser.toEntity();
@@ -63,7 +59,6 @@ public class ControllerIT {
 	public void givenNewUser_WhenPostUsers_ThenBodyContainsNewUserData (RequestSpecification spec) {
 		JSONObject requestBody = new JSONObject();
 		requestBody.put("name", "ExampleTestUser");
-		System.out.println(requestBody.toString());
 		
 		spec
 		.given()
@@ -77,6 +72,22 @@ public class ControllerIT {
 					"id", equalTo(100),
 					"name", equalTo("ExampleTestUser"),
 					"debt", equalTo(0f));
+	}
+	
+	@Test
+	public void givenNewInvalidUser_WhenPostUsers_ThenRequestError (RequestSpecification spec) {
+		JSONObject requestBody = new JSONObject();
+		requestBody.put("debt", 20f);
+		System.out.println(requestBody.toString());
+		
+		spec
+		.given()
+			.contentType("application/json")
+			.body(requestBody.toString())
+		.when()
+			.post("/api/v1/users")
+		.then()
+			.statusCode(HttpStatus.BAD_REQUEST.getCode());
 	}
 	
 }
