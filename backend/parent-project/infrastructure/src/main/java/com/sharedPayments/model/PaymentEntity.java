666113da8deb207685879e831e564de2f24f8d57
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2022-10-07 12:59:26 +0200

    feat: added lombok annotations on jpa entities

--- backend/parent-project/infrastructure/src/main/java/com/sharedPayments/model/PaymentEntity.java
+++ backend/parent-project/infrastructure/src/main/java/com/sharedPayments/model/PaymentEntity.java
@@ -1,7 +1,7 @@
 package com.sharedPayments.model;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,9 +13,14 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
 import io.micronaut.core.annotation.Introspected;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "payment")
+@Getter @Setter @EqualsAndHashCode @NoArgsConstructor
 @Introspected
 public class PaymentEntity {
 
@@ -36,112 +41,40 @@ public class PaymentEntity {
 	@NotNull
 	private Long paymentDate;
 
-	public PaymentEntity() {
-	}
-
-	public PaymentEntity(@NotNull UserEntity payer, String description, @NotNull @Positive BigDecimal price, @NotNull Long paymentDate) {
+	public PaymentEntity(@NotNull UserEntity payer, String description, @NotNull @Positive BigDecimal price,
+			@NotNull Long paymentDate) {
 		this.payer = payer;
 		this.description = description;
 		this.price = price;
 		this.paymentDate = paymentDate;
 	}
-	
-	public PaymentEntity(@NotNull UserEntity payer, String description, @NotNull @Positive double price, @NotNull Long paymentDate) {
-		this(
-			payer,
-			description,
-			BigDecimal.valueOf(price).setScale(2, RoundingMode.FLOOR),
-			paymentDate);
+
+	public PaymentEntity(@NotNull UserEntity payer, String description, @NotNull @Positive double price,
+			@NotNull Long paymentDate) {
+		this(payer, description, BigDecimal.valueOf(price).setScale(2, RoundingMode.FLOOR), paymentDate);
 	}
-	
+
 	public PaymentEntity(@NotNull UserEntity payer, String description, @NotNull @Positive double price) {
-		this(
-			payer,
-			description,
-			BigDecimal.valueOf(price).setScale(2, RoundingMode.FLOOR),
-			System.currentTimeMillis());
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public BigDecimal getPrice() {
-		return price;
+		this(payer, description, BigDecimal.valueOf(price).setScale(2, RoundingMode.FLOOR), System.currentTimeMillis());
 	}
 
 	public void setPrice(double price) {
 		this.price = BigDecimal.valueOf(price).setScale(2, RoundingMode.FLOOR);
 	}
 
-	public Long getPaymentDate() {
-		return paymentDate;
-	}
-
-	public void setPaymentDate(Long paymentDate) {
-		this.paymentDate = paymentDate;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public UserEntity getPayer() {
-		return payer;
-	}
-
-	public void setPayer(UserEntity payer) {
-		this.payer = payer;
-	}
-	
 	public static PaymentEntity fromModel(Payment payment) {
 		PaymentEntity paymentE = new PaymentEntity(
-				UserEntity.fromModel(payment.getPayer()),
+				UserEntity.fromModel(payment.getPayer()), 
 				payment.getDescription(),
-				payment.getPrice(),
-				payment.getPaymentDate()
-				);
+				payment.getPrice(), 
+				payment.getPaymentDate());
 		if (payment.getId() != null)
 			paymentE.setId(payment.getId());
-		System.out.println(paymentE.getPayer().getId());
 		return paymentE;
-		
 	}
-	
+
 	public Payment toModel() {
-		return new Payment(
-				this.id,
-				this.payer.toModel(), 
-				this.description, 
-				this.price, 
-				this.paymentDate);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(description, id, payer, paymentDate, price);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PaymentEntity other = (PaymentEntity) obj;
-		return Objects.equals(description, other.description) && Objects.equals(id, other.id)
-				&& Objects.equals(payer, other.payer) && Objects.equals(paymentDate, other.paymentDate)
-				&& Objects.equals(price, other.price);
+		return new Payment(this.id, this.payer.toModel(), this.description, this.price, this.paymentDate);
 	}
 
 }
--- backend/parent-project/infrastructure/src/main/java/com/sharedPayments/model/UserEntity.java
+++ backend/parent-project/infrastructure/src/main/java/com/sharedPayments/model/UserEntity.java
@@ -3,7 +3,6 @@ package com.sharedPayments.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,9 +15,14 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import io.micronaut.core.annotation.Introspected;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "user")
+@Getter @Setter @EqualsAndHashCode @NoArgsConstructor
 @Introspected
 public class UserEntity {
 
@@ -33,11 +37,8 @@ public class UserEntity {
 	@NotNull
 	private BigDecimal debt;
 
-	@OneToMany(mappedBy = "payer")//, cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "payer")
 	private List<PaymentEntity> payments;
-
-	public UserEntity() {
-	}
 
 	public UserEntity(@NotEmpty(message = "Cannot be empty") @Size(min = 1, max = 20) String name, BigDecimal debt) {
 		this.name = name;
@@ -46,38 +47,6 @@ public class UserEntity {
 
 	public UserEntity(@NotEmpty(message = "Cannot be empty") @Size(min = 1, max = 20) String name) {
 		this(name, new BigDecimal(0D));
-	}
-
-	public void setDebt(BigDecimal debt) {
-		this.debt = debt;
-	}
-
-	public BigDecimal getDebt() {
-		return this.debt;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public List<PaymentEntity> getPayments() {
-		return payments;
-	}
-
-	public void setPayments(List<PaymentEntity> payments) {
-		this.payments = payments;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public static UserEntity fromModel(User user) {
@@ -89,28 +58,6 @@ public class UserEntity {
 
 	public User toModel() {
 		return new User(this.id, this.name, this.debt);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(debt, id, name);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		UserEntity other = (UserEntity) obj;
-		return Objects.equals(debt, other.debt) && Objects.equals(id, other.id) && Objects.equals(name, other.name);
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", debt=" + debt + "]";
 	}
 
 }
